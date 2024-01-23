bc2d039a7dbcac3d5d48f8f88be898b07a118fb3
joaobug <joaoenfrance@gmail.com>
2024-01-23 11:46:08 -0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Joao Leal on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
